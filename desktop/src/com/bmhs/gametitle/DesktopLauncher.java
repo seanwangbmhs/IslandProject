bd40fdfdf2055fc28144abb0b95afc656848c103
rwang <rwang1@student-bmhs-la.org>
2024-01-26 12:48:55 -0800

    initial commit

--- desktop/src/com/bmhs/gametitle/DesktopLauncher.java
+++ desktop/src/com/bmhs/gametitle/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setWindowedMode(1280, 768);
 		config.setForegroundFPS(60);
+
 		config.setTitle("TileGame");
 		new Lwjgl3Application(new GameHandler(), config);
 	}
